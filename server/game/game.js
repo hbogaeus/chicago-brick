80d4862cde97c3341c0ef5e4733ae0cda0aa7c48
Quentin Vecchio <vecchioquentin@hotmail.fr>
2018-02-18 19:12:34 +0100

    Refact: server/game.js

--- server/game/game.js
+++ server/game/game.js
@@ -26,21 +26,21 @@ limitations under the License.
  *       maxPlayers (default: 4 google logo colors).
  *
  *    // game is available as a global with one method: create().
- *    var mygame = game.create('MyGame', options);
+ *    let mygame = game.create('MyGame', options);
  *
  * 2. Player info (index, controls, etc) is available from the player instances
  *    stored in Game.players.
  *
- *    var players = mygame.players;    // Sparse array of players by index.
- *    var playerMap = mygame.players;  // Map playerId -> player
+ *    let players = mygame.players;    // Sparse array of players by index.
+ *    let playerMap = mygame.players;  // Map playerId -> player
  *
  *    // Each player has an assigned index (0 - maxPlayers - 1) and an assigned
  *    // css color string.
- *    var playerFoo = mygame.playerMap[playerFooId];
+ *    let playerFoo = mygame.playerMap[playerFooId];
  *    playerFoo.color;  // -> '#4285F4'
  *    playerFoo.index;  // -> 0;
  *
- *    var player1 = mygame.players[1];  // Will be undefined in < 2 players.
+ *    let player1 = mygame.players[1];  // Will be undefined in < 2 players.
  *
  *    // Controls is a map of input -> boolean. Available controls are up, down,
  *    // left, right, a, b, x, y.
@@ -52,24 +52,24 @@ limitations under the License.
  *    Their controls fields will be updated asynchronously and you can just read
  *    them during tick() invocations.
  *
- *    mygame.on('playerJoin', function(player) { setUpPlayer(player) });
- *    mygame.on('playerQuit', function(player) { cleanUpPlayer(player) });
+ *    mygame.on('playerJoin', (player) => { setUpPlayer(player) });
+ *    mygame.on('playerQuit', (player) => { cleanUpPlayer(player) });
  *
  *    // There is also a controlsUpdate event if you need to do something more
  *    // complicated.
- *    mygame.on('controlsUpdate', function(player) { doSomething(player) });
+ *    mygame.on('controlsUpdate', (player) => { doSomething(player) });
  *
  * Games can be played by visiting the game server from a computer or mobile
  * device. They are listed by host + game in the dropdown while the game is on
  * a wall somewhere.
  */
 
-var ioClient = require('socket.io-client');
-var debug = require('debug')('wall:game');
-var os = require('os');
-var _ = require('underscore');
-var util = require('util');
-var EventEmitter = require('events');
+const ioClient = require('socket.io-client');
+const debug = require('debug')('wall:game');
+const os = require('os');
+const _ = require('underscore');
+const util = require('util');
+const EventEmitter = require('events');
 
 /**
  * An individual player.
@@ -78,7 +78,7 @@ var EventEmitter = require('events');
  * Controls are updated asynchronously and can be read directly in server module
  * code.
  */
-var Player = function(playerId, index, color) {
+let Player = (playerId, index, color) => {
   this.id = playerId;
   this.index = index;
   this.color = color;
@@ -86,37 +86,38 @@ var Player = function(playerId, index, color) {
   this.controls = {};
 };
 
+
 /**
  * This class is returned to clients who create a game using
  * GameManager.create(). Player instances are available in Game.players and
  * lifecycle events are emitted by instances of this class.  Games are
  * automatically cleaned up when modules are torn down.
  */
-var Game = function(socket, host, name, opt_options) {
+let Game = (socket, host, name, opt_options) => {
   EventEmitter.call(this);
-  var options = opt_options || {};
+  let options = opt_options || {};
 
-  var maxPlayers = options.maxPlayers || 4;
-  var colors = options.colors || ['#4285F4', '#34A853', '#FBBC05', '#EA4335'];
+  let maxPlayers = options.maxPlayers || 4;
+  let colors = options.colors || ['#4285F4', '#34A853', '#FBBC05', '#EA4335'];
 
   if (!colors.length || colors.length < 4) {
     throw new Error(
-        'Invalid game options: ' + options.maxPlayers + ' players and ' +
-        colors.length + ' colors.');
+      `Invalid game options: ${options.maxPlayers} players and ${colors.length} colors.`
+    );
   }
 
-  var players = _.map(_.range(maxPlayers), _.constant(undefined));
-  var playerMap = {};
-  var gameStateInterval = null;
+  let players = _.map(_.range(maxPlayers), _.constant(undefined));
+  let playerMap = {};
+  let gameStateInterval = null;
 
-  var game = this;
+  let game = this;
   this.players = players;
   this.playerMap = playerMap;
 
   function sendGameState() {
     socket.emit('gameState', {
       // Sending controls back makes no sense.
-      players: _.map(game.players, function(p) {
+      players: _.map(game.players, (p) => {
         return _.omit(p, 'controls');
       }),
     });
@@ -135,13 +136,13 @@ var Game = function(socket, host, name, opt_options) {
   }
 
   function provisionPlayer(playerId) {
-    var slot = _.findIndex(players, function(s) { return !s; });
+    let slot = _.findIndex(players, (s) => { return !s; });
     if (slot == -1) {
       debug('Too many players: ', playerId);
       socket.emit('errorMsg', 'Too many players.', playerId);
     } else {
-      var color = colors[slot];
-      var player = new Player(playerId, slot, color);
+      let color = colors[slot];
+      let player = new Player(playerId, slot, color);
       players[slot] = player;
       playerMap[playerId] = player;
 
@@ -153,7 +154,7 @@ var Game = function(socket, host, name, opt_options) {
 
   function removePlayer(playerId) {
     debug('Removing player: ' + playerId);
-    var player = playerMap[playerId];
+    let player = playerMap[playerId];
     delete playerMap[playerId];
     delete players[player.index];
     game.emit('playerQuit', player);
@@ -185,29 +186,29 @@ var Game = function(socket, host, name, opt_options) {
 };
 util.inherits(Game, EventEmitter);
 
-var host = '';
+let host = '';
 module.exports = {
-  init: function(flags) {
+  init: (flags) => {
     host = flags.game_server_host;
   },
-  forModule: function(forModule) {
-    var connections = [];
-    var games = [];
+  forModule: (forModule) => {
+    let connections = [];
+    let games = [];
 
     return {
       // Called by module code to add a game. Returns a new Game.
-      create: function(gameName, options) {
+      create: (gameName, options) => {
         debug('Connecting to game server.');
-        var client = ioClient('http://' + host + '/servers', {multiplex: false});
+        let client = ioClient(`http://${host}/servers`, {multiplex: false});
         connections.push(client);
 
-        var game = new Game(client, os.hostname(), gameName, options);
+        let game = new Game(client, os.hostname(), gameName, options);
         games.push(game);
         return game;
       },
 
       // Called when module is finished.
-      dispose: function() {
+      dispose: () => {
         _.invoke(connections, 'close');
       }
     };
