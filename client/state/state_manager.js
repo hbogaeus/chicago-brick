f5788e0322e57b6f1f4189e9612b6fc62f4a5c25
oskarcasselryd <oskarcasselryd@gmail.com>
2018-02-19 21:09:15 +0100

    Fix: Put 'use strict' in the right place

--- client/state/state_manager.js
+++ client/state/state_manager.js
@@ -12,9 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-'use strict';
 
 define((require) => {
+  'use strict';
   const sharedState = require('lib/shared_state');
   const debug = require('client/util/debug')('wall:state_manager');
 
