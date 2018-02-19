f8490bf683ec42749b947d36269db9be01e0025e
oskarcasselryd <oskarcasselryd@gmail.com>
2018-02-19 21:11:54 +0100

    Fix: Added 'use strict'

--- server/state/state_manager.js
+++ server/state/state_manager.js
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+'use strict';
+
 const sharedState = require('lib/shared_state');
 const debug = require('debug')('wall:state_manager');
 const _ = require('underscore');
